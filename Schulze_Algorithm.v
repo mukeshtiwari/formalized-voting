aac841f7c29dc69060dd0d222fdf7e1afbdc8358
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2016-10-25 12:56:01 +1100

    Implementation of Schulze algorithm

---
+++ Schulze_Algorithm.v
@@ -0,0 +1,21 @@
+Require Import Notations.
+Require Import Coq.Lists.List.
+Require Import Coq.Arith.Le.
+Require Import Coq.Numbers.Natural.Peano.NPeano.
+Require Import Coq.Arith.Compare_dec.
+Require Import Coq.omega.Omega.
+Require Import Bool.Sumbool.
+Require Import Bool.Bool.
+Require Import Coq.Logic.ConstructiveEpsilon.
+Require Import fp_finite_Dirk.
+Require Import Schulze_Dirk.
+Import ListNotations.
+
+Module Voting.
+
+  Parameter cand : Type.
+  Parameter ballot : list (list cand).
+
+  
+  
+End Voting.
