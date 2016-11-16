d319a1b48e0e7f651021f217da9ce7473b54ddca
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2016-11-16 18:09:31 +1100

    added john's proof

--- schulze-count.v
+++ schulze-count.v
@@ -102,7 +102,27 @@ Section Count.
     right. firstorder.
   Qed. *)
 
- 
+  (* 
+  Lemma valid_or_invalid_ballot : forall b : ballot, {ballot_valid b} + {~ballot_valid b}.
+  Proof.
+    intro.
+    unfold ballot_valid.
+    revert cand_fin.
+    cut ({(forall c : cand, In c cand_all -> b c > 0)} +
+         {~ (forall c : cand, In c cand_all -> b c > 0)}).
+    intros.
+      destruct H; firstorder.
+      induction cand_all.
+    firstorder.
+      destruct IHl.
+    destruct (le_gt_dec (b a) 0).
+      right; intro.
+        specialize (H a (or_introl eq_refl)).
+        omega.
+        left; intros.
+      destruct H; auto; congruence.
+      firstorder.
+  Qed. *)
 
   Theorem in_decidable :
     forall (b : ballot) (l : list cand),
