3aaf3932725387e68af0e64d95247345d3928e29
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2017-03-24 14:54:36 +1100

    added show instance derivation file

--- changed-code/schulze-voting/src/Cand.hs
+++ changed-code/schulze-voting/src/Cand.hs
@@ -4,7 +4,7 @@ data List a =
    Nil
  | Cons a (List a)
 
-data Cand = A | B | C | D | E deriving (Prelude.Eq, Prelude.Ord, Prelude.Show)
+data Cand = A | B | C | D | E
 
 cand_all :: List Cand
 cand_all = Cons A (Cons B (Cons C (Cons D (Cons E Nil))))
---
+++ changed-code/schulze-voting/src/Derivation.hs
@@ -0,0 +1,8 @@
+{-# Language StandaloneDeriving #-}
+module Derivation where
+import Cand
+import Lib
+
+deriving instance (Prelude.Show Cand)
+
+
